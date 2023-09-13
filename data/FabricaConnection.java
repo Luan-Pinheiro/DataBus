42b0c29447b129fa577299f5bf0598dae033b391
CarlosGilM <carlosgilmds@gmail.com>
2023-09-12 21:44:46 -0300

    ConnectionFactory

--- data/FabricaConnection.java
+++ data/FabricaConnection.java
@@ -5,8 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class FabricaConnection {
+    //Usuario do Banco de dados
+    private static final String USERNAME = "postgres";
+
+     //Usuario do Banco de dados
+    private static final String PASSWORD = "123";
+
+     //Usuario do Banco de dados
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/databus";
+
+
     public Connection getConnection() throws SQLException{
-        Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/databus", "postgres", "123");
+        Connection conexao = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
         return conexao;
     }
 }
