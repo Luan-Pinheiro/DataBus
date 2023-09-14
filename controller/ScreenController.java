cf6cabaf064798f5aee1298e410ce1933909a514
CarlosGilM <carlosgilmds@gmail.com>
2023-09-14 19:14:40 -0300

    Att JavaFx tela inicial

--- controller/ScreenController.java
+++ controller/ScreenController.java
@@ -5,12 +5,22 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class ScreenController implements Initializable {
 
-  @FXML private ImageView botaoCadastro;
-  @FXML private ImageView botaoLogin;
+  // Itens Tela Inicial
+    @FXML private ImageView botaoCadastro;
+    @FXML private ImageView botaoLogin;
+    @FXML private TextField textSenha;
+    @FXML private TextField textUsuario;
+
+    @FXML
+    void clickLogin(MouseEvent event) {
+      
+    }
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
