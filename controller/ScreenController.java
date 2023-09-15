5003684977d0b72557cf10efa77ba0a3cd59dc09
CarlosGilM <carlosgilmds@gmail.com>
2023-09-14 21:05:14 -0300

    Tela Dados Estudante

--- controller/ScreenController.java
+++ controller/ScreenController.java
@@ -2,8 +2,8 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
@@ -22,9 +22,28 @@ public class ScreenController {
   @FXML private ImageView passeCLT;
   @FXML private ImageView passeIdoso;
 
+  //Itens Tela Cadastro02
+  @FXML private ImageView botaoCadastrar;
+  @FXML private TextField textMatricula;
+  @FXML private TextField textCPF;
+  @FXML private TextField textEmail;
+  @FXML private TextField textEndereco;
+  @FXML private TextField textInstituicao;
+  @FXML private TextField textNascimento;
+  @FXML private TextField textNome;
+  @FXML private TextField textSenha2;
+  @FXML private TextField textTelefone;
+  @FXML private ImageView backgroundESTUDANTE;
+  @FXML private Group groupESTUDANTE;
+  @FXML private ImageView backgroundIDOSO;
+
+
     @FXML
     void ClickEstudante(MouseEvent event) { // Clicou no Passe Estudante
       groupCadastro01.setVisible(false);
+      groupESTUDANTE.setVisible(true);
+      backgroundESTUDANTE.setVisible(true);
+      textInstituicao.setVisible(true);
     }
 
     @FXML
@@ -36,11 +55,18 @@ public class ScreenController {
     void clickCadastro(MouseEvent event) { // Clicou no Botao de Cadastro
       groupInicialScreen.setVisible(false);
       groupInicialScreen.setDisable(true);
+      groupCadastro01.setVisible(true);
     }
 
     @FXML
     void clickIdoso(MouseEvent event) { // Cliclou no Passe Idoso
       groupCadastro01.setVisible(false);
+    }
+
+    
+    @FXML
+    void clicouBotaoCadastrar(MouseEvent event) { // Botao Cadastrar
+
     }
 
     @FXML
