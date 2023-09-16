a486ddfb6dda1a5ab0b1c293877b7b8dc73cf9ef
oantoniovinicius <devantoniovinicius@gmail.com>
2023-09-16 17:34:51 -0300

    costumer screen

--- controller/ClienteController.java
+++ controller/ClienteController.java
@@ -3,13 +3,37 @@ import model.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-public class TesteController { //TESTANDO TELA
-  TrocaTelas tT = new TrocaTelas();
+public class ClienteController { 
+  
+  @FXML
+  private ImageView botaoConsultarDados;
+  @FXML
+  private ImageView botaoConsultarRotas;
+  @FXML
+  private ImageView botaoRecarregar;
+  
+  TrocaTelas trocaTela = new TrocaTelas();
+
+  @FXML 
+  void recarregar(MouseEvent event){
+
+  }
+  
+  @FXML 
+  void consultarDados(MouseEvent event){
+    
+  }
+  
+  @FXML 
+  void consultarRotas(MouseEvent event){
+    
+  }
 
   @FXML
   void voltarCena(MouseEvent event) { //clicou na imagem voltar
-    tT.changeScreen("menu"); 
+    trocaTela.changeScreen("menu"); 
   }
 }
