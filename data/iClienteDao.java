f5681385c1864fc36d17a289f8abe56bef5b4d67
Luan-Pinheiro <luan.p12@hotmail.com>
2023-09-16 09:45:07 -0300

    modifications

--- data/iClienteDao.java
+++ data/iClienteDao.java
@@ -4,7 +4,7 @@ import model.*;
 
 public interface iClienteDao {
     ArrayList<Cliente> getAllClientes();
-    void createCliente (Cliente Cliente);
+    void createCliente (Cliente Cliente, int id);
     Cliente readCliente (long id);
     void updateCliente (Cliente Cliente);
     void deleteCliente (Cliente Cliente);
