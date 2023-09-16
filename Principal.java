5bbb1257a6eab5380318b15dd3ed3567478a08f3
CarlosGilM <carlosgilmds@gmail.com>
2023-09-16 20:48:54 -0300

    alt tela cliente

--- Principal.java
+++ Principal.java
@@ -1,16 +1,38 @@
+/* ***************************************************************
+* Autor............: Carlos Gil Martins da Silva
+* Matricula........: 202110261
+* Inicio...........: 31/08/2023
+* Ultima alteracao.: 05/09/2023
+* Nome.............: Principal
+* Funcao...........: Importa o que é necessario para iniciar
+Starta as Cenas e carrega o FXML e inicia o programa
+****************************************************************/
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import model.*;
+import controller.ScreenController; // Import Necessário para Compilação do Projet
 
-public class Principal extends Application{
-  public static void main(String[] args) {
+public class Principal extends Application {
+  private static Scene telaEntrada; // Cena inicial
 
-    launch(args);
-  }
   @Override
-  public void start(Stage primaryStage) throws Exception {
-    TrocaTelas trocaTelas = new TrocaTelas();
-    trocaTelas.start(primaryStage);
-    
+  public void start(Stage stagePrimary) throws Exception {
+
+    //stagePrimary.getIcons().add(new Image("./assets/icon.png"));//Definindo icone do programa
+
+    stagePrimary.setTitle("Camada Fisica"); // Setando Nome na barra de pesquisa
+    Parent fxmlStart = FXMLLoader.load(getClass().getResource("view/DataBus.fxml")); // Carregamento do FXML Tela
+    telaEntrada = new Scene(fxmlStart); // Definicao cena nova com o fxml carregado
+
+    stagePrimary.setScene(telaEntrada); // setando a Cena no stage
+    stagePrimary.setResizable(false); // Impossibilitando mudar tamanho da janela
+    stagePrimary.show(); // mostrando o stage
+  }
+
+  public static void main(String[] args) {
+    launch(args);
   }
 }
--- model/Passe.java
+++ model/Passe.java
@@ -1,5 +1,27 @@
 package model;
 
+import controller.ScreenController;
+
 public class Passe {
-  
+  private String email;
+  private String senha;
+  private String dataNascimento;
+  private String telefone;
+  private String nome;
+  private String endereco;
+  private String cpf;
+  static String textAlerta = "Voce digitou incorretamente os campos: ";
+
+  ScreenController sc;
+
+
+  public Cliente(String email, String senha, String dataNascimento, String numeroTel, String nome, String endereco,String cpf) {
+    this.email = email;
+    this.senha = senha;
+    this.dataNascimento = dataNascimento;
+    this.telefone = numeroTel;
+    this.nome = nome;
+    this.endereco = endereco;
+    this.cpf = cpf;
+  }
 }
